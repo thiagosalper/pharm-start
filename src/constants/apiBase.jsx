6fce8e51337d7850fd01962a5f9df449196f9d2e
Thiago Pereira <thiago_salper@hotmail.com>
2018-12-23 00:35:52 -0200

    include multilanguage

---
+++ src/constants/apiBase.jsx
@@ -0,0 +1,23 @@
+export const BASE_URL = "https://api.pharmarocket.com.br";
+
+export const HEADER_REQUEST = {
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Origin": "*"
+  }
+};
+
+export const HEADER_REQUEST_FORM = {
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Origin": "*"
+  }
+};
+
+export const API_HEADER = {
+  headers: {
+    key: "123"
+  }
+};
