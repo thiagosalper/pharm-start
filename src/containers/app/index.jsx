9e3b3685f8a290fec2f4d2a7746ae7e1841a3dd6
Thiago Pereira <thiago_salper@hotmail.com>
2018-12-26 22:35:02 -0200

    initial structure for logon

--- src/containers/app/index.jsx
+++ src/containers/app/index.jsx
@@ -1,26 +1,38 @@
 import React from "react";
-import {Route, Switch, BrowserRouter} from "react-router-dom"; 
-import {Provider} from "react-redux";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import Store from "./store";
 
 import Skeleton from "../skeleton";
+import Logon from "../logon";
 import Dash from "../dash";
 import Config from "../config";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      auth: false
+    };
+  }
   render() {
+    const { auth } = this.state;
     return (
       <Provider store={Store}>
         <BrowserRouter>
-          <Skeleton>
-            <Switch>
-              <Route exact path="/" component={Dash} />
-              <Route path="/config" component={Config} />
-            </Switch>
-          </Skeleton>
+          {auth ? (
+            <Skeleton>
+              <Switch>
+                <Route exact path="/" component={Dash} />
+                <Route path="/config" component={Config} />
+              </Switch>
+            </Skeleton>
+          ) : (
+            <Logon />
+          )}
         </BrowserRouter>
       </Provider>
-    )
+    );
   }
 }
 
---
+++ src/containers/logon/components/reset/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Reset extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Reset</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default Reset;
