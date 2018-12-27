0d20e2212e2fc5df7e2d992b531df00591fa47d3
Maurício Leal <mauricio95leal@gmail.com>
2018-12-27 19:49:51 -0300

    ajustes

--- src/containers/attendance/index.jsx
+++ src/containers/attendance/index.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
+
 import './style.css';
+import * as config from '../../utils/reactTable';
+import TableRocket from '../../components/TableRocket';
+
+const mock = [
+    {
+        
+    }
+]
 
 class Attendance extends React.PureComponent {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    render(){
-        return(
+    populateTable = (data) => {
+        data.map((obj, key) => {
+            return {
+                ...obj,
+                id: key,
+                acoes: (
+                    <div className="actions-right">
+                    </div>
+                )
+            }
+        })
+    }
+
+    render() {
+        return (
             <div>
                 Attendance
+                <TableRocket
+                    columns={config.attendanceParameters}
+                    tableData={this.state.list}
+                />
             </div>
         );
     }
-
 }
 
 export default Attendance;
